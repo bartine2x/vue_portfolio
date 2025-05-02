f6b01009933b626dce515ebf66b3536b8e7cade3
bartine2x <krabypatty2018@gmail.com>
2025-05-02 17:06:53 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,7 @@ const navigation = [
           <DisclosureButton
             class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
             <span class="absolute -inset-0.5" />
+            
             <span class="sr-only">Open main menu</span>
             <Bars3Icon v-if="!open" class="block size-6" aria-hidden="true" />
             <XMarkIcon v-else class="block size-6" aria-hidden="true" />
